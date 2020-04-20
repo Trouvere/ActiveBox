de1c80c2f721be61058bff46723a8e8b647965a1
Mishyn Aliaksei <trouvere777@mail.ru>
2020-04-20 21:45:23 +0300

    Add Smooth scroll, Nav Toggle and Slidet on slick

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
-$(function() {
+$(function () {
+  /* Fixed Header */
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
 
-    let header = $("#header");
-    let intro = $("#intro");
-    let introH;
-    let scrollPos = $(window).scrollTop();
+  checkScroll(scrollPos, introH);
 
-    $(window).on("scroll load resize", function() {
-        introH = intro.innerHeight();
-        scrollPos = $(this).scrollTop();
+  $(window).on("scroll resize", function () {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
 
-        if( scrollPos > introH ) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed");
-        }
-    });
+    checkScroll(scrollPos, introH);
+  });
 
+  function checkScroll(scrollPos, introH) {
+    if (scrollPos > introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+  /* Smooth scroll */
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70,
+      },
+      700
+    );
+  });
+
+  /* Nav Toggle */
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+
+    nav.toggleClass("show");
+  });
+
+  /* Reviews: https://kenwheeler.github.io/slick/ */
+  let slider = $("#reviewsSlider");
+
+  slider.slick({
+    // по кругу
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // затенение
+    fade: false,
+    arrows: false,
+    dots: true,
+  });
 });
